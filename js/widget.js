1a5c3c99060a70ce909e091dbcf071182685cd25
Jorge Acosta <jorgitopdr@gmail.com>
2016-05-29 22:24:17 -0400

    stage four project

--- js/widget.js
+++ js/widget.js
@@ -1,57 +1,15 @@
-var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function(){
-    if(xhr.readyState === 4){
-        var employees = JSON.parse(xhr.responseText);
-        var statusHTML = '<ul class="bulleted">';
-        for (var i=0; i<employees.length;i++){
-            if(employees[i].inoffice === true){
-            statusHTML += '<li class="in">'; 
-            }
-         else{
-            statusHTML += '<li class="out">';
-            }
-            statusHTML += employees[i].name;
-            statusHTML += '</li>';
-        }
-        statusHTML += '</ul>';
-        document.getElementById('employeeList').innerHTML = statusHTML;
-    }
-};
-
-xhr.open('GET', 'data/employees.json');
-xhr.send();
-
-
-
-var xhr2 = new XMLHttpRequest();
-xhr2.onreadystatechange = function(){
-    if(xhr2.readyState === 4){
-        var rooms = JSON.parse(xhr2.responseText);
-        var statusHTML = '<ul class="rooms">';
-        for (var i = 0; i<rooms.length;i++){
-            if(rooms[i].available === true){
-                statusHTML += '<li class="empty">';
-            }else{
-                statusHTML += '<li class="full">';
-            }
-            statusHTML += rooms[i].room;
-            statusHTML += '</li>';
-        }
-        statusHTML += '</ul>';
-        document.getElementById('roomList').innerHTML = statusHTML;
-    }
-}
-
-xhr2.open('GET', 'data/rooms.json');
-xhr2.send();
-
-
-
-
-
-
-
-
-
-
-
+$(document).ready(function () {
+  $.getJSON('../data/employees.json', function (data) {
+    var statusHTML = '<ul class="bulleted">';
+    $.each(data,function (index, employee) {
+      if (employee.inoffice === true) {
+        statusHTML +='<li class="in">';
+      } else {
+        statusHTML +='<li class="out">';
+      }
+      statusHTML += employee.name + '</li>';
+    });
+    statusHTML += '</ul>';
+    $('#employeeList').html(statusHTML)
+  }); // end getJSON
+}); // end ready
